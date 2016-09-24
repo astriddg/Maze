4980e991d838af6959c8cf1fda30c77f2586e7f1
astriddg <astriddegaste@gmail.com>
2016-09-24 21:18:14 +0100

    Kinda first commit of my whole project...

---
+++ game.py
@@ -0,0 +1,94 @@
+
+# -*-coding:Utf-8 -*
+
+TOP_RIGHT = "   |"
+EMPTY = "    "
+BOTTOM_RIGHT = "_ _|"
+BOTTOM_NO_RIGHT = "_ _ "
+NO_BOTTOM_RIGHT = "   |"
+
+SOUTH_AND_EAST = 1
+SOUTH = 2
+EAST = 3
+NONE = 4
+
+from maze import Maze
+
+class Game:
+
+
+	def __init__(self, rows, cols) :
+		self.maze = Maze(rows, cols)
+		self.maze.generate()
+		self.grid = {}
+		self.rows = rows
+		self.cols = cols
+
+	def __repr__(self):
+		for i in range(0, self.cols + 1):
+			self.grid[(0, i)] = BOTTOM_NO_RIGHT
+		for j in range(0, self.rows * 2 + 1):
+			self.grid[(j, 0)] = " |"
+		self.grid[(0, 0)] = "  "
+
+
+
+		return self.setGrid()
+
+	def setGrid(self):
+		for r in range(0, self.rows):
+			for c in range (0, self.cols):
+				check_state = self.checkState(self.maze.grid[(r, c)])
+				if check_state == SOUTH_AND_EAST:
+					self.grid[(r * 2 + 1, c+1)] = TOP_RIGHT # problème à régler avec c et r.
+					self.grid[((r + 1) * 2, c+1)] = BOTTOM_RIGHT
+				if check_state == SOUTH:
+					self.grid[(r * 2 + 1, c+1)] = EMPTY
+					self.grid[((r + 1) * 2, c+1)] = BOTTOM_NO_RIGHT
+				if check_state == EAST:
+					self.grid[(r * 2 + 1, c+1)] = TOP_RIGHT
+					self.grid[((r + 1) * 2, c+1)] = NO_BOTTOM_RIGHT
+				if check_state == NONE:
+					self.grid[(r * 2 + 1, c+1)] = EMPTY
+					self.grid[((r + 1) * 2, c+1)] = EMPTY
+		
+		final_string = " "
+		for r in range(0, self.rows * 2 + 1):
+			row_string = ""
+			for c in range(0, self.cols + 1):
+				row_string += self.grid[(r, c)]
+			final_string += row_string + "\n"
+		return final_string
+
+
+
+	def checkState(self, cell):
+		if cell.row == self.rows-1 and cell.col == self.cols-1:
+			return SOUTH_AND_EAST
+		elif cell.row == self.rows-1:
+			if cell.e_wall:
+				return SOUTH_AND_EAST
+			else:
+				return SOUTH
+		elif cell.col == self.cols-1:
+			if cell.s_wall:
+				return SOUTH_AND_EAST
+			else:
+				return EAST
+		else:
+			if cell.e_wall and cell.s_wall:
+				return SOUTH_AND_EAST
+			elif cell.e_wall:
+				return EAST
+			elif cell.s_wall:
+				return SOUTH
+			else:
+				return NONE
+
+
+
+game = Game(20, 20)
+
+print(game)
+
+
---
+++ maze.py
@@ -0,0 +1,96 @@
+
+
+import random
+
+N = (-1, 0)
+S = (1, 0)
+W = (0, -1)
+E = (0, 1) 
+
+class Cell:
+
+	def __init__(self, row, col):
+		self.row = row
+		self.col = col
+
+		self.n_wall = True
+		self.s_wall = True
+		self.e_wall = True
+		self.w_wall = True
+
+	def closed(self):
+		return self.n_wall and self.s_wall and self.e_wall and self.w_wall
+
+
+
+class Maze:
+
+
+	def __init__(self, rows, cols) :
+		self.rows = rows
+		self.cols = cols
+		self.grid = {}
+		self.path = []
+
+	def generate(self):
+		self.reset()
+		self.total = self.rows * self.cols
+		self.current_cell = self.start
+		self.visited = 1
+		cell_stack = []
+
+		while self.visited < self.total:
+			
+			neighbours = self.findNeighbours(self.current_cell)
+			if len(neighbours) >= 1: # may not work
+				neighbour = random.choice(neighbours)
+				print(neighbour[1])
+				if neighbour[1] == N:
+					self.current_cell.n_wall = False
+					neighbour[0].s_wall = False
+				elif neighbour[1] == S:
+					self.current_cell.s_wall = False
+					neighbour[0].n_wall = False
+				elif neighbour[1] == W:
+					self.current_cell.w_wall = False
+					neighbour[0].e_wall = False
+				elif neighbour[1] == E:
+					self.current_cell.e_wall = False
+					neighbour[0].w_wall = False
+				cell_stack.append(self.current_cell)
+				self.current_cell = neighbour[0]
+				self.visited += 1
+			else:
+				self.current_cell = cell_stack.pop()
+			
+		print(self.visited)
+
+		if self.current_cell == self.end:
+			self.path = cell_stack[:]
+
+		self.path.append(self.end)
+
+
+
+	def reset(self):
+		for r in range(self.rows):
+			for c in range(self.cols):
+				self.grid[(r, c)] = Cell(r, c)
+		self.start = self.grid[(0, 0)] 
+		self.end = self.grid[(self.rows-1, self.cols-1)]
+
+	def findNeighbours(self, current_cell):
+		directions = [N, S, W, E]
+		neighbours = []
+		for d in directions:
+			temp_r, temp_c = current_cell.row + d[0], current_cell.col + d[1]
+			if temp_r >= 0 and temp_c >= 0 and temp_r < self.rows and temp_c < self.cols:
+				if self.grid[(temp_r, temp_c)].closed():
+					" mon chien"
+					neighbour = self.grid[(temp_r, temp_c)]
+					neighbours.append((neighbour, d))
+		return neighbours
+
+
+
+		
